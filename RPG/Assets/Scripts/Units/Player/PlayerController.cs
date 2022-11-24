b47cebbecd62b2ffce13f871fb531c75397246bd
AtapinAV <atap03081991@mail.ru>
2022-11-24 23:04:23 +0400

    Add movement

--- RPG/Assets/Scripts/Units/Player/PlayerController.cs
+++ RPG/Assets/Scripts/Units/Player/PlayerController.cs
@@ -7,7 +7,23 @@ namespace RPG.Units.Player
     public class PlayerController : MonoBehaviour
     {
         private StatsAssistant _stats;
+
+        private void Update()
+        {
+            var ad = Input.GetAxis("Horizontal");
+            if (ad != 0)
+            {
+                transform.position += _stats.GetSpeed() * ad * Time.deltaTime * Vector3.right;
+            }
+
+            var ws = Input.GetAxis("Vertical");
+            if (ws != 0)
+            {
+                transform.position += _stats.GetSpeed() * Time.deltaTime * ws * Vector3.forward;
+            }
+        }
     }
+
 }
 
 
